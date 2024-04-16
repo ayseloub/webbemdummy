84388318932041e08ca2eae9e6b1eebf211b843d
notataxpayer <dymikavindra725@gmaiil.com>
2024-04-16 10:45:44 +0700

    Setup react router dom & Tailwindcss

---
+++ src/page/Compponent/Contoh.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contoh = () => {
+  return (
+    <div>
+      <div className="text-5xl text-red-700">
+        BIKIN DISINI (BIKIN FILE BARU SESUAI DENGAN BAGIAN PEKERJAAN)
+      </div>
+    </div>
+  );
+};
+
+export default Contoh;
---
+++ src/page/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Contoh from "./Compponent/Contoh";
+
+const Home = () => {
+  return <Contoh />;
+};
+
+export default Home;
