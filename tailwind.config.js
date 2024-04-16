b874ab2dfbca43a4c911bcdc527d431c5401844a
pijenn <rafiezein2110@gmail.com>
2024-04-16 18:28:07 +0700

    buat config warna

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+
+      },
+      colors:{
+        "cust-green": "#208B71",
+        "cust-lime": "#BCDB37",
+        "cust-blue": "#004159",
+        "cust-orange" : "#FE7201",
+        "cust-background" : "#FDFADB",
+        "cust-white" : "#FFFEFB",
+        "cust-gray" : "#929292",
+        "cust-artikel" : "#208B71",
+      },
+    },
   },
   plugins: [],
 };
