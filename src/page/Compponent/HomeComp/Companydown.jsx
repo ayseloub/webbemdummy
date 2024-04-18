db7aa4ac24ae95e776271183628f9f8e8dedc4c2
Royhan-1 <129375214+Royhan-1@users.noreply.github.com>
2024-04-19 01:29:43 +0700

    tes visibility

--- src/page/Compponent/HomeComp/Companydown.jsx
+++ src/page/Compponent/HomeComp/Companydown.jsx
@@ -2,15 +2,16 @@ import React from "react"
 
 const Company = (props) => {
     return (
-        <div className="flex flex-col w-full my-2">
-            <div className="">
-                <h2 className="relative font-sans">{props.heading}</h2>
-                //!add garis dibawah heading nanti
-                <div className=""></div>
+        <div className="flex flex-col w-full h-auto my-2 bg-cyan-700 items-center">
+            <div className="max-w-[10%]">
+                <h2 className="relative font-sans text-white font-bold text-center">{props.heading}</h2>
+                {/* //!add garis dibawah heading nanti */}
+                <div className="relative"></div>
             </div>
-            <div className="border border-cust-green rounded-lg my-2">
-                //!tambahin buat company profile & magazine nanti
+            <div className="border border-cust-green rounded-lg my-4 w-9/12 h-100">
+                
             </div>
+            {/* //!tambahin buat company profile & magazine nanti */}
         </div>
     )
 
