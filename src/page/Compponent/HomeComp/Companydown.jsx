5ea32d16cab96275e18eddac96848775e0b20eb4
Royhan-1 <129375214+Royhan-1@users.noreply.github.com>
2024-04-19 14:33:22 +0700

    Tes tambah container

--- src/page/Compponent/HomeComp/Companydown.jsx
+++ src/page/Compponent/HomeComp/Companydown.jsx
@@ -2,14 +2,16 @@ import React from "react"
 
 const Company = (props) => {
     return (
-        <div className="flex flex-col w-full h-auto my-2 bg-cyan-700 items-center">
-            <div className="max-w-[10%]">
-                <h2 className="relative font-sans text-white font-bold text-center">{props.heading}</h2>
+        <div className="flex flex-col w-full max-w-[1700px] h-auto m-0 p-8 bg-cust-background items-center justify-center">
+            <div className="max-w-max pb-4">
+                <h2 className="relative font-sans text-cust-artikel font-bold text-center pb-2">{props.heading}</h2>
                 {/* //!add garis dibawah heading nanti */}
-                <div className="relative"></div>
+                <div className="relative h-1 border border-cust-green">
+                    
+                </div>
             </div>
-            <div className="border border-cust-green rounded-lg my-4 w-9/12 h-100">
-                
+            <div className="border border-cust-green rounded-lg m-0 p-0.5 w-9/12 h-4/6">
+                <iframe className="relative border border-white mx-auto w-full rounded-lg" src=""></iframe>
             </div>
             {/* //!tambahin buat company profile & magazine nanti */}
         </div>
