8ca1eead4d78227e82a822030a8122f365079ba2
Royhan-1 <129375214+Royhan-1@users.noreply.github.com>
2024-04-21 00:12:12 +0700

    benerin path image kabinet di comp Kabinetpic + coba taruh component2 di contoh.jsx

--- src/page/Compponent/Contoh.jsx
+++ src/page/Compponent/Contoh.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import Company from "./HomeComp/Companydown";
+import Magazine from "./HomeComp/Valuemag";
+import Kabinetpic from "./HomeComp/Kabinetpic";
+
 
 const Contoh = () => {
   return (
     <div>
       <div className="text-5xl text-cust-green">
         BIKIN DISINI (BIKIN FILE BARU SESUAI DENGAN BAGIAN PEKERJAAN)
+        <Company heading="Company Prof" />
+        <Magazine heading="Value Mag" />
+        <Kabinetpic />
       </div>
     </div>
   );
