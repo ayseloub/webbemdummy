330e51be61652234be5c8246c3eb0cf16624f870
Royhan-1 <129375214+Royhan-1@users.noreply.github.com>
2024-04-20 23:40:03 +0700

    add comp gambar kabinet

---
+++ src/page/Compponent/HomeComp/Kabinetpic.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Kabinetpic = () => {
+    return (
+        <div className="flex flex-col w-full max-w-[1920px] h-auto mt-0 mb-15 p-8 bg-cust-background items-center justify-center">
+            <div className="border border-transparent rounded-lg m-0 p-0.5 w-8/12 h-auto max-h-96 aspect-video">
+                <img src="../../assets/kabinetArthakara"></img>
+            </div>
+        </div>
+    )
+}
+
+export default Kabinetpic
