b30565a61fb0fed22c6afea0176e3869651ba01d
ayseloub <azlopulalan@gmail.com>
2024-04-30 20:52:29 +0700

    push aza

--- src/page/Compponent/HomeComp/Landing.jsx
+++ src/page/Compponent/HomeComp/Landing.jsx
@@ -3,11 +3,12 @@ import Segment4 from "./segment4";
 import Contoh from "../Contoh";
 import Tentang from "./Tentang";
 import Artikel from "./Artikel";
+// import Sambutan from "./Sambutan";
 
 const Landing = () => {
   return (
     <div>
-      <Contoh />
+      {/* <Contoh /> */}
       <Tentang />
       <Artikel/>
       <Segment4 />
