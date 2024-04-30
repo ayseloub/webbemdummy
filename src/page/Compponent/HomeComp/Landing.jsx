0352667b6931d6fe3932f0f878366ecd6016c835
ayseloub <azlopulalan@gmail.com>
2024-04-30 20:39:09 +0700

    push branch pijen

--- src/page/Compponent/HomeComp/Landing.jsx
+++ src/page/Compponent/HomeComp/Landing.jsx
@@ -3,11 +3,14 @@ import Segment4 from "./segment4";
 import Contoh from "../Contoh";
 import Tentang from "./Tentang";
 import Artikel from "./Artikel";
+import Sambutan from "./Sambutan";
+
 
 const Landing = () => {
   return (
     <div>
       <Contoh />
+      <Sambutan />
       <Tentang />
       <Artikel/>
       <Segment4 />
