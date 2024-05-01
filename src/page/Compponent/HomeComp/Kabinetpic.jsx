4645ecd432cde4b3175a979f3d9cc4ed3c16a967
Royhan-1 <129375214+Royhan-1@users.noreply.github.com>
2024-05-01 10:05:20 +0700

    fix sesuai revisian + tambah bg

--- src/page/Compponent/HomeComp/Kabinetpic.jsx
+++ src/page/Compponent/HomeComp/Kabinetpic.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import kabinetArthakara from "../../../assets/kabinetArthakara.png";
+import kabinetPicRev from "../../../assets/kabinetPicRev.svg";
 
 const Kabinetpic = () => {
     return (
-        <div className="flex flex-col w-full max-w-[1920px] h-auto mt-0 mb-15 p-8 bg-cust-background items-center justify-center">
-            <div className="border border-transparent rounded-lg m-0 p-0.5 w-8/12 h-auto max-h-96">
-                <img src={kabinetArthakara} alt="Gambar Kabinet Arthakara"/>
+        <div className="flex flex-col w-full lg:max-w-[1920px] h-auto mt-0 mb-0 bg-cust-background items-center justify-center">
+            <div className="flex flex-col w-full lg:bg-[url('https://res.cloudinary.com/dnoqmou6x/image/upload/v1714531235/bgPicKab_dvtqbz.png')] mx-auto items-center justify-center">
+                <div className="rounded-lg m-0 p-0.5 w-11/12 h-auto pt-16 lg:pt-20">
+                    <img src={kabinetPicRev} alt="Gambar Kabinet Arthakara"/>
+                </div>
             </div>
         </div>
     )
--- src/page/Compponent/HomeComp/Valuemag.jsx
+++ src/page/Compponent/HomeComp/Valuemag.jsx
@@ -1,24 +1,26 @@
 import React from "react"
-import Headingmag from "./Headingmag.jsx"
+import headMag from "../../../assets/headMag.svg"
 
 const Magazine = (props) => {
     return (
-        <div className="flex flex-col w-full max-w-[1920px] h-auto mt-0 mb-15 p-8 bg-cust-background items-center justify-center">
-            <div className="max-w-max pb-4">
-                <Headingmag />
-                {/* <h2 className="relative font-lato text-8xl text-cust-green font-bold text-center pb-8">{props.heading}</h2> */}
-                {/* //!add garis dibawah heading nanti */}
-                {/* <div className="relative mx-auto w-22vw h-0.5 bg-cust-lime max-h-max">
-                    <div className="mx-auto -translate-y-[3px] w-1/12vw h-2 bg-cust-green"></div>
-                </div> */}
-            </div>
-            <div className="border border-transparent rounded-lg m-0 p-0.5 w-8/12 h-auto max-h-96 aspect-video">
-                <iframe className="fp-iframe align-middle block relative mx-auto my-0 border border-cust-green w-full h-full rounded-lg"
-                 src={props.embed}
-                 allowFullScreen
-                >
-                    {/* tes flipbook */}
-                </iframe>
+        <div className="flex flex-col w-full lg:max-w-[1920px] h-auto mt-0 mb-0 lg:p-0 bg-cust-background items-center justify-center">
+            <div className="flex flex-col w-full lg:bg-[url('https://res.cloudinary.com/dnoqmou6x/image/upload/v1714531632/bgMagfix_eqbhix.png')] mx-auto items-center justify-center">
+                <div className="max-w-max mb-8 pt-16 lg:mb-12 lg:pt-20">
+                    <img src={headMag} alt=""/>
+                    {/* <h2 className="relative font-lato text-8xl text-cust-green font-bold text-center pb-8">{props.heading}</h2> */}
+                    {/* //!add garis dibawah heading nanti */}
+                    {/* <div className="relative mx-auto w-22vw h-0.5 bg-cust-lime max-h-max">
+                        <div className="mx-auto -translate-y-[3px] w-1/12vw h-2 bg-cust-green"></div>
+                    </div> */}
+                </div>
+                <div className="rounded-lg m-0 p-0.5 w-11/12 h-auto max-h-[580px] aspect-video">
+                    <iframe className="fp-iframe align-middle block relative mx-auto my-0 border border-cust-green w-full h-full rounded-lg"
+                    src={props.embed}
+                    allowFullScreen
+                    >
+                        {/* tes flipbook */}
+                    </iframe>
+                </div>
             </div>
         </div>
     )
